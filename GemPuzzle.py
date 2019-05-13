1a4ed643b0763709062af4a765bd00c673559f4d
Artur <avagyanart@yandex.ru>
2019-05-13 23:18:50 +0300

    python

--- GemPuzzle.py
+++ GemPuzzle.py
@@ -1,8 +1,17 @@
 import tkinter
 import os
 
+IMG_PATH = 'nums'
+SIZE = 4
+
 main_window = tkinter.Tk()
 main_window.title('Gem Puzzle')
+
+files_list = os.listdir(IMG_PATH)
+files_list_path = []
+for _file in files_list:
+    files_list_path.append(os.path.join(IMG_PATH, _file))
+print(files_list_path)
 label_1 = tkinter.Label(main_window, text = 'Hello World!')
 label_1.pack()
 main_window.mainloop()
